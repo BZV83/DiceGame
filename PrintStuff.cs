c5b61eb6e3a0f9e1aae3eeca1a6ad5b429e72601
BZV83 <bzvspry@gmail.com>
2024-01-17 14:05:55 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreC_Fun
+{
+    internal class PrintStuff
+    {
+        private string lang;
+
+        public PrintStuff()
+        {
+            lang = "EN";
+        }
+
+        public PrintStuff(string temp)
+        {
+            lang = temp;
+        }
+        public void PrintName(string fName)
+        {
+            if (lang == "EN")
+            {
+                System.Console.WriteLine("Hello, " + fName);
+            }
+
+            if (lang == "TAG")
+            {
+                System.Console.WriteLine("Kumusta, " + fName);
+            }
+        }
+    }
+}
